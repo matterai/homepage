b77c4f9fd105ff35c6854d3b393ccb467825538e
matterai <matterai.net@gmail.com>
2022-09-26 18:15:31 -0400

    Added daisyui

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,7 +1,18 @@
 function App() {
   return (
-    <div className="App">
-      App
+    <div className="container mx-auto">
+      <div className="navbar">
+        <div className="flex-1">
+          <div className="btn btn-ghost normal-case text-xl">@matterai</div>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal p-0">
+            <li><a>Works</a></li>
+            <li><a>Posts</a></li>
+            <li><a>GitHub</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- client-app/tailwind.config.js
+++ client-app/tailwind.config.js
@@ -2,15 +2,25 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  daisyui: {
+    themes: ["light", "dark"],
+  },
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      screens: {
+        sm: '640px',
+        md: '768px',
+      }
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
