fc339e2fb71651b117b7f4b3b3ce094d683493c4
matterai <matterai.net@gmail.com>
2022-09-28 18:13:19 -0400

    Pretty fixes.

--- client-app/src/components/Header.tsx
+++ client-app/src/components/Header.tsx
@@ -6,14 +6,27 @@ export default class Header extends Component {
         return (
             <div className="navbar sticky top-0">
                 <div className="flex-1">
-                    <div className="btn btn-ghost normal-case text-xl">@matterai</div>
+                    <div className="btn btn-ghost normal-case text-xl">
+                        @matterai
+                    </div>
                 </div>
                 <div className="flex-none">
                     <ul className="menu menu-horizontal p-0">
-                        <li><a href="/">Works</a></li>
-                        <li><a href="/">Posts</a></li>
                         <li>
-                            <a href="/"><GitHubIcon /> GitHub</a>
+                            <a href="/">
+                                Works
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                Posts
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <GitHubIcon /> 
+                                GitHub
+                            </a>
                         </li>
                     </ul>
                 </div>
